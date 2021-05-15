0d3eeb94c329fe0ad3d3c56d70aac83e2267ecb1
Shasha Yan <shashayan@gmail.com>
2021-05-15 18:15:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+def initialized(name)
+  @name = name
+end 
+
+GRADES = [ ]
+
+def add_student(name, grade)
+  school.add_student.each do |name, grade|
+   GRADES << (grade) unless GRADES.include?(grade)
+ end
+end
+
+def grade(grade)
+  school.grade.collect do |grade|
+    grade.all(grade)
+  end
+end
+
+def sort(name, grade)
+  student.sort 
+end
