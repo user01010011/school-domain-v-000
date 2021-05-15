7666281eb0f0ebd2faa8f3812f67dc90de71a42f
Shasha Yan <shashayan@gmail.com>
2021-05-15 18:35:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,22 +6,22 @@ def initialize(name)
   @roster = { }
 end
 
-# def add_student(student_name, grade)
-#   roster[grade].add_student.each do |student_name|
-#    roster[grade] << student_name
-#  end
-# end
+def add_student(student_name, grade)
+  roster[grade].add_student.each do |student_name|
+   roster[grade] << student_name
+ end
+end
 #
 # def grade(grade)
 #   roster[grade]
 # end
 #
-def sort(grade, student_name)
-  sorted = { }
-  roster.each do |grade, student_name|
-    sorted[grade] = student_name.sort
-  end
-  sorted
-end
+# def sort(grade, student_name)
+#   sorted = { }
+#   roster.each do |grade, student_name|
+#     sorted[grade] = student_name.sort
+#   end
+#   sorted
+# end
 #
 end
