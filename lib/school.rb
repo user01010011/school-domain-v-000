679358fc1043e00fbe53efb556cb064ad425d96a
Shasha Yan <shashayan@gmail.com>
2021-05-15 18:37:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,9 +7,8 @@ def initialize(name)
 end
 
 def add_student(student_name, grade)
-  roster[grade].add_student.each do |student_name|
-   roster[grade] << student_name
- end
+  roster[grade] ||= [ ]
+  roster[grade] << student_name
 end
 #
 # def grade(grade)
